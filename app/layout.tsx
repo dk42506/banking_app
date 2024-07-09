442e5fc83051b72bc25c49660c5e297adf9d57fe
Dylan <dylan.keller6205@gmail.com>
2024-07-08 22:27:01 -0400

    update: title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
+  title: "Syzygy",
   description: "Syzygy is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
